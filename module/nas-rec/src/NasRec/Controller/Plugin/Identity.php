3c5a5cc4aa9f3a5f5b09bff778df19c6448e25f2
Rafi Adnan <rafi@nascenia.com>
2014-11-11 12:19:26 +0600

    add identity() controller plugin

---
+++ module/nas-rec/src/NasRec/Controller/Plugin/Identity.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Rafi Adnan <rafi@nascenia.com>
+ * @since 2014-11-10
+ * @version 2014-11-10
+ */
+
+namespace NasRec\Controller\Plugin;
+
+use NasRec\Authentication\Identity\IdentityProvider;
+use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+
+class Identity extends AbstractPlugin
+{
+    /**
+     * @var IdentityProvider
+     */
+    protected $identityProvider;
+
+    public function __construct(IdentityProvider $identityProvider)
+    {
+        $this->identityProvider = $identityProvider;
+    }
+
+    public function __invoke()
+    {
+        return $this->identityProvider->getIdentity();
+    }
+} 
