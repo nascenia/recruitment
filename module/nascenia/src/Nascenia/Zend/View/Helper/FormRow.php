9639ec5740003bc94395ee9203388c25f92d5d76
Rafi Adnan <rafi@nascenia.com>
2014-11-12 12:01:35 +0600

    include label attributes and error messages for form controls

--- module/nascenia/src/Nascenia/Zend/View/Helper/FormRow.php
+++ module/nascenia/src/Nascenia/Zend/View/Helper/FormRow.php
@@ -8,19 +8,33 @@
 namespace Nascenia\Zend\View\Helper;
 
 use Zend\Form\ElementInterface;
+use Zend\Form\LabelAwareInterface;
 use Zend\Form\View\Helper\FormRow as ZendFormRow;
 
 class FormRow extends ZendFormRow
 {
     public function render(ElementInterface $element)
     {
+        if ($element instanceof LabelAwareInterface) {
+            $element->setLabelAttributes(array_merge(
+                $element->getLabelAttributes()
+                , array('class' => 'control-label')
+            ));
+        }
+
         $labelHtml = $element->getLabel() ? $this->view->formLabel($element) : '';
         $inputHtml = $this->view->formElement($element);
 
+        $errorClass = $element->getMessages() ? 'has-error' : '';
+        $errorHtml = $this->view->formElementErrors($element, array(
+            'class' => 'help-block',
+        ));
+
         return <<<HTML
-<div class="form-group">
+<div class="form-group {$errorClass}">
     {$labelHtml}
     {$inputHtml}
+    {$errorHtml}
 </div>
 HTML;
 
